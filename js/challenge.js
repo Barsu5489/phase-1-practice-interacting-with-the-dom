9cf57f4607c3b2739112c2e0ac5337675d3e89a6
barsu5489 <ebarsulai@gmail.com>
2022-12-27 15:50:26 +0300

    Comments can now be added

--- js/challenge.js
+++ js/challenge.js
@@ -25,6 +25,21 @@ function timeDisp(){
         
     })
 
+    const form = document.getElementById('comment-form')
+    const list = document.getElementById('list')
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        const comment = e.target.comment.value
+        if(comment.length >0){
+            console.log(comment)
+            const p = document.createElement('p')
+            p.textContent = comment
+            list.appendChild(p)
+
+        }
+       
+    })
+
 
 document.addEventListener('DOMContentLoaded',()=>{
     setInterval(timeDisp,1000)
