2d02b47beb31628a1c9a89523edd0ecdd2d2cdaf
barsu5489 <ebarsulai@gmail.com>
2022-12-27 15:33:08 +0300

    Paused and play count

--- js/challenge.js
+++ js/challenge.js
@@ -1,11 +1,30 @@
 
 
 let h1 = document.getElementById('counter')
+let ispaused = false
 h1.textContent = 0
 
 function timeDisp(){
+    if(!ispaused){
     h1.textContent++
+    }
 }
+    let pause = document.getElementById('pause')
+    pause.addEventListener('click',(e)=>{
+      e.preventDefault()
+        if(pause.textContent  === 'pause'){
+            ispaused = true
+        pause.textContent = 'Resume'
+        } else if(pause.textContent === 'Resume'){
+            ispaused = false
+                pause.textContent = 'pause'
+                
+               
+            
+        }
+        
+    })
+
 
 document.addEventListener('DOMContentLoaded',()=>{
     setInterval(timeDisp,1000)
